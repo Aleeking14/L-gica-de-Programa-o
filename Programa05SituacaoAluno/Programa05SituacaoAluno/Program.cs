a8c03d8975d879574163bacaad004f0e2ee8ba60
Aleeking14 <juninho.galocura22@hotmail.com>
2026-07-28 21:46:25 -0300

    Add files via upload

---
+++ Programa05SituacaoAluno/Programa05SituacaoAluno/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Programa05SituacaoAluno
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string nome;
+            double nota1;
+            double nota2;
+            double media;
+
+            Console.WriteLine("=================================");
+            Console.WriteLine(" SITUAÇÃO DO ALUNO");
+            Console.WriteLine("=================================");
+            Console.WriteLine();
+            Console.Write("Digite o nome do aluno: ");
+            nome = Console.ReadLine();
+            Console.Write("Digite a primeira nota: ");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite a segunda nota: ");
+
+
+            nota2 = double.Parse(Console.ReadLine());
+            media = (nota1 + nota2) / 2;
+            Console.WriteLine();
+            Console.WriteLine("Aluno: " + nome);
+            Console.WriteLine("Média: " + media.ToString("F2"));
+            
+            if (media >= 7)
+            {
+              Console.WriteLine("Situação: APROVADO");
+            }
+            else if (media >= 5)
+            {
+              Console.WriteLine("Situação: RECUPERAÇÃO");
+            }
+            else
+            {
+              Console.WriteLine("Situação: REPROVADO");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para finalizar.");
+            Console.ReadKey();
+		}
+	}
+}
