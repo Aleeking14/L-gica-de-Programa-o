e32c4fa45a734f79119d57a059a7538949f1781e
Aleeking14 <juninho.galocura22@hotmail.com>
2026-07-28 21:41:38 -0300

    Add files via upload

---
+++ Programa01OlaMundo/Programa01OlaMundo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Programa01OlaMundo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("====================================");
+            Console.WriteLine(" INTRODUÇÃO À PROGRAMAÇÃO EM C#");
+            Console.WriteLine("====================================");
+            Console.WriteLine();
+            Console.WriteLine("Olá! Este é o meu primeiro programa.");
+            Console.WriteLine("Estou aprendendo lógica de programação.");
+            Console.WriteLine("Turma: HT-SIS-01-M-26");
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para finalizar.");
+            Console.ReadKey();
+		}
+	}
+}
---
+++ Programa01OlaMundo/Programa02CadastroAluno/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alex Junio
+ * Date: 28/07/2026
+ * Time: 21:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Programa02CadastroAluno
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
