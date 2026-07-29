b992bc676fd404115344319d33b18c40000f1154
Aleeking14 <juninho.galocura22@hotmail.com>
2026-07-29 09:55:49 -0300

    Add files via upload

---
+++ Programa08Tabuada/Programa08Tabuada/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Programa08Tabuada
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			 int numero;
+             int resultado;
+
+             Console.WriteLine("========");
+             Console.WriteLine(" TABUADA");
+             Console.WriteLine("========");
+             Console.WriteLine();
+
+             Console.Write("Digite um número: ");
+             numero = int.Parse(Console.ReadLine());
+             Console.WriteLine();
+
+             Console.WriteLine("Tabuada do número " + numero);
+             Console.WriteLine();
+
+             for (int contador = 1; contador <= 10; contador++)
+             {
+               resultado = numero * contador;
+               Console.WriteLine(numero + " x " + contador + " = " + resultado);
+             }
+             Console.WriteLine();
+
+             Console.WriteLine("Pressione qualquer tecla para finalizar.");
+             Console.ReadKey();
+		}
+	}
+}
