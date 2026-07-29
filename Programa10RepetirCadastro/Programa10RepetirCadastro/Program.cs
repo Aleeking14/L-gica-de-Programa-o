09422eeae1b9451dae5d99bf54f188cbc10d5e8b
Aleeking14 <juninho.galocura22@hotmail.com>
2026-07-29 09:56:43 -0300

    Add files via upload

---
+++ Programa10RepetirCadastro/Programa10RepetirCadastro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Programa10RepetirCadastro
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string nome;
+            string continuar;
+
+            do
+            {
+              Console.Clear();
+              Console.WriteLine("=================================");
+              Console.WriteLine(" CADASTRO DE PESSOA");
+              Console.WriteLine("=================================");
+              Console.WriteLine();
+
+              Console.Write("Digite o nome: ");
+              nome = Console.ReadLine();
+              Console.WriteLine();
+
+              Console.WriteLine("Pessoa cadastrada: " + nome);
+              Console.WriteLine();
+              Console.Write("Deseja realizar outro cadastro? S/N: ");
+              continuar = Console.ReadLine();
+            }
+             while (continuar.ToUpper() == "S");
+             Console.WriteLine();
+
+             Console.WriteLine("Programa finalizado.");
+             Console.ReadKey();
+		}
+	}
+}
