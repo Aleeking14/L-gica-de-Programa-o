fae13f2f5ff21ffb15a008e80b80c678d875711f
Aleeking14 <juninho.galocura22@hotmail.com>
2026-07-29 09:56:21 -0300

    Add files via upload

---
+++ Programa09ControleSenha/Programa09ControleSenha/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Programa09ControleSenha
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string senhaDigitada;
+            string senhaCorreta = "1234";
+
+            Console.WriteLine("===================");
+            Console.WriteLine(" CONTROLE DE ACESSO");
+            Console.WriteLine("===================");
+            Console.WriteLine();
+
+            Console.Write("Digite a senha: ");
+            senhaDigitada = Console.ReadLine();
+            while (senhaDigitada != senhaCorreta)
+            {
+              Console.WriteLine();
+              Console.WriteLine("Senha incorreta.");
+              Console.Write("Digite a senha novamente: ");
+              senhaDigitada = Console.ReadLine();
+            }
+              Console.WriteLine();
+
+              Console.WriteLine("Senha correta.");
+              Console.WriteLine("Acesso permitido.");
+              Console.WriteLine();
+
+              Console.WriteLine("Pressione qualquer tecla para finalizar.");
+              Console.ReadKey();
+		}
+	}
+}
