870325113f9bc8321970373821ab028a1762437a
Aleeking14 <juninho.galocura22@hotmail.com>
2026-07-28 21:43:45 -0300

    Add files via upload

---
+++ Programa02CadastroAluno/Programa02CadastroAluno/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Programa02CadastroAluno
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string nome;
+            int idade;
+            string curso;
+
+            Console.WriteLine("=================================");
+            Console.WriteLine(" CADASTRO DE ALUNO");
+            Console.WriteLine("=================================");
+            Console.WriteLine();
+            Console.Write("Digite o nome do aluno: ");
+            nome = Console.ReadLine();
+            Console.Write("Digite a idade do aluno: ");
+            idade = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do curso: ");
+            curso = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("CADASTRO REALIZADO");
+            Console.WriteLine("Nome: " + nome);
+
+
+            Console.WriteLine("Idade: " + idade + " anos");
+            Console.WriteLine("Curso: " + curso);
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para finalizar.");
+            Console.ReadKey();
+		}
+	}
+}
